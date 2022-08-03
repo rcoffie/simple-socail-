1d18c799b15380c33e40307fdc1da23cde63b6e1
Robert Coffie <rcoffie22@yahoo.com>
2022-08-03 23:23:30 +0000

    worked on index page and post form

--- post_engine/urls.py
+++ post_engine/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from post_engine import views
 
 urlpatterns = [
-     path("index", views.index, name="index"),  
-     path('create_post', views.create_post, name="create_post"),
+     path("index", views.index, name="index"),
+     # path('create_post', views.create_post, name="create_post"),
      path('post_detail/<str:pk>/', views.post_detail, name="post_detail"),
 ]
 
