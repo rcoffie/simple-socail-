06d38c26e0f996c45af61b4946a199470ab53dba
Robert Coffie <rcoffie22@yhahoo.com>
2022-08-08 21:01:19 +0000

    working on messages

--- account_engine/forms.py
+++ account_engine/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
 from account_engine.models import Profile
 
 
@@ -22,6 +22,37 @@ class RegistrationForm(UserCreationForm):
             "password1",
             "password2",
         )
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise  ValidationError("Username already exists")
+            return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise  ValidationError("Email already exists")
+            return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password don't match")
+
+            return password2
+
+            def save(self, commit=True):
+                user = User.objects.create_user(
+                self.cleaned_data['username'],
+                self.cleaned_data['email'],
+                self.cleaned_data['password1']
+                )
+                return user
 
 
 class UserEditForm(forms.ModelForm):
